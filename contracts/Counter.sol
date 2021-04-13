5bbcadf0f0c440ed4d02ec37ec70712670073ff2
Lorran Sutter <sutterlorran@gmail.com>
2021-04-13 15:39:21 -0400

    Update contract | add ABI

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -1,25 +1,21 @@
 //// SPDX-License-Identifier: MIT
 
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity ^0.8.0;
 
 contract Counter {
-    uint256 private count;
+    mapping(address => uint256) private counts;
 
-    constructor() public {
-        count = 0;
-    }
+    constructor() {}
 
     function increment() public {
-        require(count + 1 >= count, "Overflow");
-        count += 1;
+        counts[msg.sender] += 1;
     }
 
     function decrement() public {
-        require(count >= 1, "Underflow");
-        count -= 1;
+        counts[msg.sender] -= 1;
     }
 
     function getCount() public view returns (uint256) {
-        return count;
+        return counts[msg.sender];
     }
 }
